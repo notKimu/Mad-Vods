58994ee35114cdb7558481b8ab5763d216ab6763
Kimu <kimu@Kimus-MacBook-Pro.local>
2023-05-31 23:28:17 +0200

    Second version: JSON list and main page started

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
 import Header from "./components/Header";
+import styles from "./styles/app.module.css";
+// Vod list
+import vods from "./files/vods.json";
 
 function MainPage() {
+    const vodList = Object.values(vods);
+    const lastVod = vodList.slice(-1);
+
     return (
         <>
             <Header />
+
+            <div className="container">
+                <div className={styles.lastVodContainer}>
+                    <h1>Último VOD</h1>
+                    <div className={styles.lastVodPlayer}>
+                        <iframe
+                            className={styles.lastVod}
+                            src={lastVod[0].url}
+                            allow="autoplay"
+                            title="Last VOD"
+                        />
+                        <div className={styles.lastVodInfo}>
+                            <h2>{lastVod[0].title}</h2>
+                            <span>{lastVod[0].description}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 };
